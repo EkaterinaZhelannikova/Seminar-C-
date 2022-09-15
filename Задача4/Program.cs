61b5f96cf23647291f6b097764b0636b3a630fea
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2022-09-15 22:08:28 +0300

    задача4

---
+++ Задача4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int num = Math.Abs(int.Parse(Console.ReadLine()));
+
+
+if((num < 1000) & (num > 99))
+{
+    Console.WriteLine(num % 10);
+}
+else
+{
+    Console.WriteLine("Число не соответствует условию задачи");
+}
