3e707f6ef6efe2c26522253b0d0ab490062e581d
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2022-09-15 22:00:01 +0300

    задача3

---
+++ Задача3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int count = -num;
+
+while(count <= num)
+{
+    Console.Write($"{count} ");
+    count++;
+}
