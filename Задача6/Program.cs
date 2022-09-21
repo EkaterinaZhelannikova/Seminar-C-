ab2ea4e6a4b943857e24000ecd1ca9f74b575322
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2022-09-21 16:16:59 +0300

    Задача6

---
+++ Задача6/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+//456 -> 46
+
+Console.Clear();
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+
+Console.Write($"{firstDigit * 10 + thirdDigit}");
