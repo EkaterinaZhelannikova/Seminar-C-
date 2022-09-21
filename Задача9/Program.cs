bcab863549a79ac4075fc32d290f41672e8969ae
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2022-09-21 17:10:02 +0300

    Задача9

---
+++ Задача9/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход два числа и проверяет,
+//является ли одно число квадратом другого.
+ 
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if(number1 == number2 * number2)
+{
+    Console.WriteLine($"Число {number1} является квадратом числа {number2}");
+}
+else if(number2 == number1 * number1)
+{
+    Console.WriteLine($"Число {number2} является квадратом числа {number1}");
+}
+else
+{
+    Console.WriteLine("Числа НЕ являются квадратами друг друга");
+}
