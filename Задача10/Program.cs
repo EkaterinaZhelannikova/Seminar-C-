42116cc44f7567ab776879758c7f4bc996c08006
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2022-09-24 13:19:42 +0300

    Задача10

---
+++ Задача10/Program.cs
@@ -0,0 +1,32 @@
+﻿//Напшите программу, которая принимает на вход координаты точки (X и Y),
+//причем X не равно 0 и Y не равно 0 и выдыет номер четверти, в которой
+//находится эта точка.
+
+Console.Clear();
+Console.Write("Введите X: ");
+int X = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите Y: ");
+int Y = int.Parse(Console.ReadLine()!);
+
+if (X > 0) {
+    if (Y > 0) 
+    {
+        Console.WriteLine("1");
+    }
+    else
+    {
+        Console.WriteLine("4");
+    }
+}
+else
+{
+    if(Y > 0)
+    {
+        Console.WriteLine("2");
+    }
+    else
+    {
+        Console.WriteLine("3");
+    }
+}
