83fba896af54219555bba3822175db43cd76dc15
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2022-09-26 20:25:26 +0300

    Задача14

---
+++ Задача14/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, число, создает массив данного размера, заполняет его нулями и единицами
+//в случайном порядке и выводит в консоль.
+// 8 -> [1,0,1,1,0,1,0,0]
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int arrayLength = int.Parse(Console.ReadLine()!);
+
+int[] array = new int[arrayLength];
+
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] array)
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(0, 2);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
