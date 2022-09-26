195d352eb61e39bf95ff902b7c5c4020e5c9c791
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2022-09-26 15:48:48 +0300

    Задача13

---
+++ Задача13/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напииште программу, которая принимает на вход число (N)
+//и выдает таблицу квадратов чисел от 1 до N
+//5 -> 1, 4, 5, 16, 25
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int count = 1;
+
+while(count <= n)
+{
+    Console.Write(Math.Pow(count, 2));
+    if(count != n)
+    {
+        Console.Write(", ");
+    }
+    count++;
+}
