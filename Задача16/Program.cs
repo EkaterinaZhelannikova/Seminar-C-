ebea7bd6241154aae12ca6c1636370055f8c8e62
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2022-10-01 22:18:43 +0300

    Задача13

--- Задача16/Program.cs
+++ Задача16/Program.cs
@@ -6,23 +6,49 @@ Console.Clear();
 Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine()!);
 
-int[] array = {6, 7, 19, 345, 3};
+int[] array = CreateArray();
 string result = String.Empty;
-void Function (int[] array)
+PrintArray(array);
+FindNumber(array, num);
+Console.Write(result);
+
+int[] CreateArray()
+{
+    int[] array = {6, 7, 19, 345, 3};
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(array[i]);
+        if (i == count - 1)
+        {
+            Console.Write("]");
+        }
+        else
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine();
+}
+
+void FindNumber (int[] array, int num)
 {
     for (int i = 0; i < array.Length; i++)
     {
        if(num == array[i])
        {
-        result = "Да";
+        result = $"Число {num} присутствует в массиве";
         break;
        }
        else
        {
-        result = "Нет";
+        result = $"Числа {num} нет в массиве";
        }
     }
 }
-Function(array);
-
-Console.Write(result);
