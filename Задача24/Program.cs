fef80742f353bb455f2688e628036b443f1768f1
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2022-10-09 14:56:34 +0300

    Задача24

---
+++ Задача24/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход число и выдает
+//количество цифр в числе. Использовате рекурсию
+//456 -> 3
+
+Console.Clear();
+
+Console.Write($"Введите число: ");
+
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"В числе {num} число знаков равно {CountDigits(num)}");
+
+int CountDigits(int number, int counter = 1)
+{
+    if (number / 10 == 0)
+    {
+        return counter;
+    }
+    else
+    {
+        return counter + CountDigits(number / 10);
+    }
+}
