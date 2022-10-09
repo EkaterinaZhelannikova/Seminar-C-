27240e0b15108379d925193496e616aeff17e17e
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2022-10-09 14:47:29 +0300

    Задача23

---
+++ Задача23/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход число (А)
+//и выдает сумму чисел от 1 до А. Использовать рекурсию
+//7 -> 28
+
+Console.Clear();
+
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Sum: {RecSum(number)}");
+
+
+int RecSum (int number)
+{
+    if (number > 1)
+    {
+        return number + RecSum(--number) ;
+    }
+    else
+    {
+        return 1;
+    }
+}
