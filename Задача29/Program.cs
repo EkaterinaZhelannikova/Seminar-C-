9d72f010b25b03ab3ce4998073d5979af77e4752
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2022-10-09 23:20:12 +0300

    Задача29

---
+++ Задача29/Program.cs
@@ -0,0 +1,47 @@
+﻿//Задайте двумерный массив. Найдите элементы, у которых оба индекса четные
+//и замените эти элементы на их квадраты
+
+Console.Clear();
+ 
+int[,] array = new int[5, 5];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+ReplaceArray(array);
+PrintArray(array);
+ 
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+ 
+void FillArray(int[,] array)
+{
+    Random generator = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = generator.Next(1,10);
+        }
+    }
+}
+
+void ReplaceArray(int[,] array)
+{
+    for (int i = 2; i < array.GetLength(0); i+=2)
+    {
+        for (int j = 2; j < array.GetLength(1); j+=2)
+        {
+            array[i, j] = array[i, j] * array[i, j];
+        }
+
+    }
+}
