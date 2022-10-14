69fd4639728be581843e6738cf715851b0120612
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2022-10-14 17:03:25 +0300

    Задача33

---
+++ Задача33/Program.cs
@@ -0,0 +1,18 @@
+﻿// Составить частотный словарь элементов двумерного массива. Частотный словарь 
+//содержит информацию о том, сколько раз встречается элемент входных данных.
+
+Console.Write("введите превое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+Console.Write("введите длину последовотельности: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.Write($"{num1} {num2} ");
+for (int i = 1; i <= n-2 ; i++)
+{
+    int num3 = num1 + num2;
+    Console.Write($"{num3} ");
+    num1 = num2;
+    num2 = num3;
+}
